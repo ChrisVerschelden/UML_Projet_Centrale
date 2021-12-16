9f56e12a57e38a5167c3fb5dbc7de9edf54cdf6a
mateoisabey <77398592+mateoisabey@users.noreply.github.com>
2021-12-16 12:22:07 +0100

    Actionneur/Capteur init

--- Actionneur.java
+++ Actionneur.java
@@ -1,3 +1,18 @@
-public class Actionneur {
+public class Actionneur extends Object{
+
+    private int id ;
+    private String Etats ;
+    private int valeur ;
+
+    public Actionneur(int id,String Etats,int valeur) {
+        this.id = id ;
+        this.Etats = Etats;
+        this.valeur = valeur;
+    }
+
+    public void faire() {
+        
+    }
+
     
 }
--- Capteur.java
+++ Capteur.java
@@ -1,3 +1,18 @@
-public class Capteur {
-    
+public class Capteur extends Object{
+    private int id ;
+    private String Etats ;
+    private int valeur ;
+
+    public Capteur(int id,String Etats,int valeur) {
+        this.id = id ;
+        this.Etats = Etats;
+        this.valeur = valeur;
+    }
+
+    public void selfCheck() {
+        
+    }
+    public void valeurCritiqueDetecte() {
+        
+    }
 }

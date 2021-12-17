2bd902100b89ebc65bae36843325d1344ce132a3
mateoisabey <77398592+mateoisabey@users.noreply.github.com>
2021-12-17 10:02:35 +0100

    Capteur/Actionneur update

--- UML_Projet_Centrale/Capteur.java
+++ UML_Projet_Centrale/Capteur.java
@@ -8,27 +8,50 @@ public class Capteur implements Objet{
     private long freq ;
     private int valeurCritique ;
     private Centrale c ;
+    
+    public void timer() throws InterruptedException {
+        int x = 1 ;
+        while (x != 2) {
+            Thread.sleep(this.freq); // Wait 10 seconds
+            selfCheck();
+        }
+    }
+    
 
-    public Capteur(long id,int etats,int valeur, long freq) {
+    public Capteur(long id,int etats,int valeur, long freq) throws InterruptedException {
         this.id = id ;
         this.etats = etats;
         this.valeur = valeur;
         this.freq = freq ;
-        this.valeurCritique = 30;       
+        this.valeurCritique = 30;
+        timer();
+    }
+    public long getId() {
+        return this.id;
+    }
+    public int getEtat() {
+        return this.etats;
+    }
+    public int getValeur() {
+        return this.valeur;
+    }
+    public Centrale getCentrale() {
+        return this.c ;
+    }
+
+    public void SetCentrale(Centrale c) {
+        this.c = c ;
     }
 
     public void selfCheck() {
-        if (this.etats == 1) {
-            System.out.println("Le Capteur marche");
-            update(c);
+        if (this.etats == 1) { //fonctionne
+            update();
         }
-        else if (this.etats == 3) {
-            System.out.print("Le Capteur est a une valeur critique");
+        else if (this.etats == 3) { //disfonctionnel
             valeurCritiqueDetecte();
         }
-        else if (this.etats == 2) {
-            System.out.println("Le capteur ne fonctionne pas");
-            update(c);
+        else if (this.etats == 2) { // en mode off
+            update();
         }
         
     }
@@ -39,7 +62,7 @@ public class Capteur implements Objet{
     }
 
     @Override
-    public void update(Centrale c) {
+    public void update() {
         Random r = new Random();
         int low = 10;
         int high = 100;
@@ -48,7 +71,7 @@ public class Capteur implements Objet{
     }
 
     @Override
-    public void notifyCentrale(Object o) {
-        System.out.println(this.etats);
+    public void notifyCentrale() {
+        
     }
 }

95e8ca4548e1d49157466fea373b52e6ad7994ea
mateoisabey <77398592+mateoisabey@users.noreply.github.com>
2021-12-17 09:40:28 +0100

    implementation Actionneur/Capteur

--- UML_Projet_Centrale/Actionneur.java
+++ UML_Projet_Centrale/Actionneur.java
@@ -1,19 +1,35 @@
 package UML_Projet_Centrale;
+import java.util.Random;
 
 public class Actionneur implements Objet{
 
     private long id ;
-    private String Etats ;
+    private int etats ;
     private int valeur ;
 
-    public Actionneur(long id,String Etats,int valeur) {
+    public Actionneur(long id,int etats,int valeur) {
         this.id = id ;
-        this.Etats = Etats;
+        this.etats = etats;
         this.valeur = valeur;
     }
 
     public void faire() {
+        System.out.print("L'actionneur fais ce qu'il doit faire");
+    }
 
+    @Override
+    public void update(Centrale c) {
+        Random r = new Random();
+        int low = 10;
+        int high = 100;
+        int randomNumber = r.nextInt(high-low) + low;
+        this.valeur = randomNumber ;
+        
+    }
+
+    @Override
+    public void notifyCentrale(Object o) {
+        System.out.println(this.etats);
     }
 
     
--- UML_Projet_Centrale/Capteur.java
+++ UML_Projet_Centrale/Capteur.java
@@ -1,22 +1,54 @@
 package UML_Projet_Centrale;
+import java.util.Random;
 
 public class Capteur implements Objet{
     private long id ;
-    private String etats ;
+    private int etats ;
     private int valeur ;
     private long freq ;
+    private int valeurCritique ;
+    private Centrale c ;
 
-    public Capteur(long id,String etats,int valeur, long freq) {
+    public Capteur(long id,int etats,int valeur, long freq) {
         this.id = id ;
         this.etats = etats;
         this.valeur = valeur;
         this.freq = freq ;
+        this.valeurCritique = 30;       
     }
 
     public void selfCheck() {
+        if (this.etats == 1) {
+            System.out.println("Le Capteur marche");
+            update(c);
+        }
+        else if (this.etats == 3) {
+            System.out.print("Le Capteur est a une valeur critique");
+            valeurCritiqueDetecte();
+        }
+        else if (this.etats == 2) {
+            System.out.println("Le capteur ne fonctionne pas");
+            update(c);
+        }
         
     }
     public void valeurCritiqueDetecte() {
-        
+        if (this.valeur >= valeurCritique) {
+            this.etats = 3 ;
+        }
+    }
+
+    @Override
+    public void update(Centrale c) {
+        Random r = new Random();
+        int low = 10;
+        int high = 100;
+        int randomNumber = r.nextInt(high-low) + low;
+        this.valeur = randomNumber ;
+    }
+
+    @Override
+    public void notifyCentrale(Object o) {
+        System.out.println(this.etats);
     }
 }
